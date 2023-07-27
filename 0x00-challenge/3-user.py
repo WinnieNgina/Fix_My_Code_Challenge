484ea071aaf2115ec7c799d011df2cc8268808eb
Winfred <winfrednginakilonzo@gmail.com>
2023-07-27 14:41:33 +0300

    return true

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -55,7 +55,7 @@ class User():
         elif self.__password is None:
             return False
         else:
-            return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+            return True
 
 if __name__ == '__main__':
     print("Test User")
